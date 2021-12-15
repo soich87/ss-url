156d67f522f2fbf37601e4a6e661f6bf6873dee9
soichuong <chuonganhtai0277@gmail.com>
2021-12-15 10:59:01 +0700

    shorting service and CRUD a shorten url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'dashboard#index'
+  root 'shorten_urls#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/:url_code', to: 'shorten_urls#redirection', as: 'redirection'
+  resources :shorten_urls
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_044332) do
+ActiveRecord::Schema.define(version: 2021_12_14_101456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shorten_urls", force: :cascade do |t|
+    t.string "url_code", null: false
+    t.string "origin_url", null: false
+    t.datetime "expired_at"
+    t.bigint "user_id"
+    t.integer "clicked_count", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
